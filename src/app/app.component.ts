2c774c5962320000268ae6e16aeba633a50084e2
Olar-Alex-Student <alex.olar@student.usv.ro>
2024-11-13 18:00:07 +0200

    add footer and edit header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/login/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],  // Import RouterOutlet for routing
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],  // Import RouterOutlet for routing
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
